273aa79baa5b8d2229d76924dc51f3b36a3ccd6b
anuja <anujachougule1677@gmail.com>
2024-09-30 20:57:06 +0530

    all files added

---
+++ Day1/ifelse.java
@@ -0,0 +1,14 @@
+class Ifelse{
+	public static void main(String args[]){
+		int a = 0;
+		if(a == 0){
+			System.out.println("number is zero");
+		}
+		else if(a>0){
+			System.out.println("number is positive");
+		}
+		else{
+			System.out.println("number is negetive");
+		}
+	}
+}
