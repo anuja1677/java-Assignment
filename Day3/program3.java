8db383a9a594e55a9a43172149d07162c0d4ca3e
Sakshi <jadhavsakshi930@gmail.com>
2024-10-02 23:08:14 +0530

    all files added

---
+++ Day3/program3.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String args[]){
+		for(int i=1;i<=4;i++){
+			if(i%2==0){
+				System.out.print("1 ");
+			}else{
+				System.out.print("0 ");
+			}
+		}
+	}
+}
