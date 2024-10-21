bca3a81d112f7693a81266ab922e52939170bbb7
Sakshi <jadhavsakshi930@gmail.com>
2024-10-21 23:45:16 +0530

    add one file

---
+++ Day12/program4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Demo{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt;
+		int b = sc.nextInt;
+		string c = sc.next;
+		System.out.println(a, c, b);
+	}
+}
---
+++ Day13/program4.java
@@ -0,0 +1,9 @@
+class Demo{
+	public static void main(String args[]){
+		int a=1;
+		for(int i=1;i<=5;i++){
+			a = a*i;
+		}
+		System.out.println("the factorial of 5 is:"+fact);
+	}
+}
